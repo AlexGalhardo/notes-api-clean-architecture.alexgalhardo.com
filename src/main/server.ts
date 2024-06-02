7b0ca10b214a3bdabf2d7de76a6bca44b9c16e15
Alex Galhardo <aleexgvieira@gmail.com>
2024-06-02 11:28:10 -0300

    fix: set host to 0.0.0.0

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,13 +2,13 @@ import "module-alias/register";
 import { MongoHelper } from "@/external/repositories/mongodb/helpers/mongo-helper";
 
 MongoHelper.connect(process.env.MONGO_URL)
-    .then(async () => {
-        const app = (await import("./config/app")).default;
-        app.get("/", (req, res) => {
-            res.json({ success: true, message: "API is on, lets goo!" });
-        });
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`...Notes API Clean Architecture is running on http://localhost:${process.env.PORT || 3000}`);
-        });
-    })
-    .catch(console.error);
+	.then(async () => {
+		const app = (await import("./config/app")).default;
+		app.get("/", (req, res) => {
+			res.json({ success: true, message: "API is on, lets goo!" });
+		});
+		app.listen(Number(process.env.PORT || 3000), '0.0.0.0', () => {
+			console.log(`...Notes API Clean Architecture is running on http://localhost:${process.env.PORT || 3000}`);
+		});
+	})
+	.catch(console.error);
